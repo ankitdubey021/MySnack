786c80c2264d17506d44edb20166786f2cfae787
Ankit Dubey <ankitdubey021@gmail.com>
2017-11-01 17:09:28 +0530

    Initial commit

---
+++ app/src/main/java/lucifer/org/librarydemo/MainActivity.java
@@ -0,0 +1,13 @@
+package lucifer.org.librarydemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
